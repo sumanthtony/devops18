7f25b59459d384c02edae8b484fa6529970a8cf8
sumanthtony <sumanthbattagiri99@gmail.com>
2025-04-25 13:38:53 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "mustafashaik77.devops.project.bucket"
+  bucket = "sumanth77.devops.project.bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -20,12 +20,5 @@ resource "aws_s3_bucket_versioning" "three" {
 bucket = aws_s3_bucket.one.id
 versioning_configuration {
 status = "Enabled"
-}
-}
-terraform {
-backend "s3" {
-region = "us-east-1"
-bucket = "mustafashaik77.devops.project.bucket"
-key = "prod/terraform.tfstate"
 }
 }
