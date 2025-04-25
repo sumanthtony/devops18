43f1b3bec205a203fb2c8992457a5086e8d36268
sumanthtony <sumanthbattagiri99@gmail.com>
2025-04-25 13:58:56 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -22,3 +22,12 @@ versioning_configuration {
 status = "Enabled"
 }
 }
+
+terraform {
+backend "s3" {
+region = "ap-south-1"
+bucket = "sumanth77.devops.project.bucket"
+key = "prod/terraform.tfstate"
+}
+}
+
