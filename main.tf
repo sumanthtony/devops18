d5dae4f74e98c337c73d6b136b2e837b43481454
sumanthtony <sumanthbattagiri99@gmail.com>
2025-04-25 13:33:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-0454e52560c7f5c55"
+    image_id           = "ami-091dccf4e2d272bae"
     vpc_security_group_ids = [aws_security_group.web_server.id]
-    instance_type = "t2.micro"
-    key_name = "lastone"
+    instance_type = "t2.medium"
+    key_name = "terraform-key-pair"
     tags = {
         Name = "DevOps"
     }
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-024286466e258f339", "subnet-09558309acf4661a8"]
+     subnets = ["subnet-031ad9126ef3288c7", "subnet-045c62c226b065a80"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
@@ -33,7 +33,7 @@ resource "aws_autoscaling_group" "web_server_asg" {
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    availability_zones    = ["us-east-1a", "us-east-1b"] 
+    availability_zones    = ["ap-south-1a", "ap-south-1b"] 
     launch_template {
         id      = aws_launch_template.web_server_as.id
         version = "$Latest"
